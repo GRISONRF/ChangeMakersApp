735ef70dde46650fa8eedd9f693941729a967d7c
grisonrf <grisonrf@gmail.com>
2022-07-11 17:10:38 -0600

    3 tests passed

--- tests.py
+++ tests.py
@@ -53,31 +53,6 @@ class FlaskTestsLoggedOut(TestCase):
         app.config['TESTING'] = True
         self.client = app.test_client()
 
-    def test_login(self):
-        """Test log in form.
-
-        Unlike login test above, 'with' is necessary here in order to refer to session.
-        """
-
-        with self.client as v:
-            result = v.post('/login',
-                            data={'volunteer_id':'1', 'fname':'Ana', 'lname':'Costa', 'v_email':'ana_costa@gmail.com', 'v_password':'test123', 'v_city':'Sacramento', 'v_state':'California', 'v_pic':'/static/images/volunteer-icon.PNG'},
-                            follow_redirects=True
-                            )
-            self.assertEqual(session['volunteer_id'], '1')
-            self.assertIn(b"Your information", result.data)
-
-
-            with self.client as i:
-                result = i.post('/login',
-                                data={'inst_name':'Denver Rescue Mission', 'inst_email':'drm@gmail.com', 'inst_password':'test123', 'inst_address':'6100 Smith Rd, Denver, CO 80216', 'inst_city':'Denver', 'inst_state':'Colorado', 'inst_lat':39.773578, 'inst_long':-104.917346, 'inst_pic':'/static/images/volunteer-icon.PNG'},
-                                follow_redirects=True
-                                )
-                self.assertEqual(session['inst_id'], '1')
-                self.assertIn(b"Your information", result.data)
-
-
-
     def test_volunteer_logout(self):
         """Test logout route for volunteer."""
 
