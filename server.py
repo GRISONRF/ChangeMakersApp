56f567477d5f35329b4800cba2a30e98409ec79a
grisonrf <grisonrf@gmail.com>
2022-07-17 15:01:48 -0600

    Small styling changes

--- server.py
+++ server.py
@@ -37,12 +37,8 @@ def homepage():
     """view homepage"""
 
     all_inst = crud.get_all_institutions()
-    inst_pic_hp = []
-    for inst in all_inst:
-        if inst.inst_pic != "/static/images/volunteer-icon.PNG":
-            inst_pic_hp.append(inst)
         
-    return render_template('homepage.html', inst_pic_hp=inst_pic_hp)
+    return render_template('homepage.html', all_inst=all_inst)
 
 
 # ---------------- REGISTER PAGE ----------------
